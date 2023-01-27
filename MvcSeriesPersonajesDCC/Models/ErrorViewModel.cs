1ab5a98ac2e5b61f31a9011d58ce4b3383af2028
DomingoCastro <domingocastro1998@outlook.com>
2023-01-27 18:41:23 +0100

    Add project files.

---
+++ MvcSeriesPersonajesDCC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcSeriesPersonajesDCC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
